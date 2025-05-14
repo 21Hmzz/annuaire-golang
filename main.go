e79f9e6b2a7ba4f76f3361a21ee67420c2c1d177
21Hmzz <hamza.bella2107@gmail.com>
2025-05-14 15:31:42 +0200

    ajout de la fonction ListerContacts et gestion des actions via flag

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 type Contact struct {
 	Nom string
 	Tel string
@@ -7,8 +12,27 @@ type Contact struct {
 
 var annuaire = make(map[string]Contact)
 
+func ListerContacts() {
+	if len(annuaire) == 0 {
+		fmt.Println("Annuaire vide.")
+		return
+	}
+	for _, c := range annuaire {
+		fmt.Printf("- %s : %s\n", c.Nom, c.Tel)
+	}
+}
+
 func main() {
 	annuaire["Hamza"] = Contact{Nom: "Hamza", Tel: "0601020303"}
 	annuaire["Valentin"] = Contact{Nom: "Valentin", Tel: "0603040506"}
 	annuaire["Serhat"] = Contact{Nom: "Serhat", Tel: "0602340406"}
+
+	action := flag.String("action", "", "actions possible : ajouter, rechercher, lister, supprimer, modifier")
+	flag.Parse()
+	switch *action {
+	case "lister":
+		ListerContacts()
+	default:
+		fmt.Println("Action non reconnue. Utilisez --action avec : ajouter, rechercher, lister, supprimer, modifier")
+	}
 }
