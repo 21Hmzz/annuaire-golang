63228f440cff1061dbeee9af92cb2e713a4341e5
21Hmzz <hamza.bella2107@gmail.com>
2025-05-14 15:40:10 +0200

    ajout de tests pour la fonction AjouterContact

--- main_test.go
+++ main_test.go
@@ -1,8 +1,28 @@
 package main
 
-func init() {
+import (
+	"fmt"
+	"testing"
+)
+
+func initAnnuaire() {
 	annuaire = make(map[string]Contact)
 	annuaire["Hamza"] = Contact{Nom: "Hamza", Tel: "0601020303"}
 	annuaire["Valentin"] = Contact{Nom: "Valentin", Tel: "0603040506"}
 	annuaire["Serhat"] = Contact{Nom: "Serhat", Tel: "0602340406"}
+}
+
+func TestAjouterContact(t *testing.T) {
+	initAnnuaire()
+	AjouterContact("Lukhas", "0707070707")
+	fmt.Println("Liste des contacts après ajout :")
+	ListerContacts()
+
+	contact, ok := annuaire["Lukhas"]
+	if !ok {
+		t.Error("Lukhas n'a pas été ajouté")
+	}
+	if contact.Tel != "0707070707" {
+		t.Errorf("Numéro incorrect pour Lukhas. Attendu: 0707070707, Obtenu: %s", contact.Tel)
+	}
 }
