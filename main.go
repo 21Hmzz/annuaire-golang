a3d0b1f8e0bb556d6083051123a4a4a4f4416078
21Hmzz <hamza.bella2107@gmail.com>
2025-05-14 15:28:03 +0200

    ajout annuaire + init 3 contacts

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ type Contact struct {
 	Tel string
 }
 
-func main() {
+var annuaire = make(map[string]Contact)
 
+func main() {
+	annuaire["Hamza"] = Contact{Nom: "Hamza", Tel: "0601020303"}
+	annuaire["Valentin"] = Contact{Nom: "Valentin", Tel: "0603040506"}
+	annuaire["Serhat"] = Contact{Nom: "Serhat", Tel: "0602340406"}
 }
